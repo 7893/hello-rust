4c67e4d6984f1778efc4a852a87fa9e912e63ede
7893 <etc@is20.net>
2021-06-25 01:36:46 +0800

    variables demo

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,24 @@
 fn main() {
-    let mut x =5;
-    let x=x+1;
-    let x=x*2;
-    println!("The value of x is:{}",x);
-    //x=6;
-    println!("The value of x is:{}",x);
+    let mut x = 5;
+    x = x + 1;
+
+    //x=6
+    println!("The value of x is:{}", x);
+
+    let x = x * 2;
+
+    //x=12
+    println!("The value of x is:{}", x);
     println!("Hello, world!");
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+
+    //y=y+2
+    println!("The value of y is:{}", y);
+
+    let spaces = "        ";
+    let spaces = spaces.len();
+    println!("The vaule of spaces is:{}", spaces)
 }
