a52a8c1bff5344494740e4cf2c9deae2758c27fc
7893 <etc@is20.net>
2021-06-27 04:14:19 +0800

    slice dome

--- slices/target/debug/deps/slices-79ef2d2edb03515e.d
+++
@@ -1,5 +0,0 @@
-D:\src\hello-rust\slices\target\debug\deps\slices-79ef2d2edb03515e.exe: src\main.rs
-
-D:\src\hello-rust\slices\target\debug\deps\slices-79ef2d2edb03515e.d: src\main.rs
-
-src\main.rs:
--- slices/target/rls/debug/deps/slices-ca0991c731ba5981.d
+++
@@ -1,5 +0,0 @@
-d:\src\hello-rust\slices\target\rls\debug\deps\slices-ca0991c731ba5981.rmeta: src\main.rs
-
-d:\src\hello-rust\slices\target\rls\debug\deps\slices-ca0991c731ba5981.d: src\main.rs
-
-src\main.rs:
