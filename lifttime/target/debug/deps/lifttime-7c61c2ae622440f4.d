04f0114ed72a6300c8c0e6239b32606bf667e7de
7893 <etc@is20.net>
2021-07-04 09:16:08 +0800

    lifetime demos

---
+++ lifttime/target/debug/deps/lifttime-7c61c2ae622440f4.d
@@ -0,0 +1,5 @@
+D:\src\hello-rust\lifttime\target\debug\deps\lifttime-7c61c2ae622440f4.exe: src\main.rs
+
+D:\src\hello-rust\lifttime\target\debug\deps\lifttime-7c61c2ae622440f4.d: src\main.rs
+
+src\main.rs:
---
+++ lifttime/target/debug/lifttime.d
@@ -0,0 +1,1 @@
+D:\src\hello-rust\lifttime\target\debug\lifttime.exe: D:\src\hello-rust\lifttime\src\main.rs
