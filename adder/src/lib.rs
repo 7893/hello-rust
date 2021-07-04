73e5be43f9f7a886edd31542218975fad1c39f8c
7893 <etc@is20.net>
2021-07-04 10:04:21 +0800

    tests demos

---
+++ adder/src/lib.rs
@@ -0,0 +1,70 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn gerrting(_name: &str) -> String {
+    format!("Hello {}!", _name)
+    //format!("Hello")
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}",
+                value
+            )
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}",
+                value
+            )
+        }
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_works_two() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+        assert_ne!(5, add_two(2));
+    }
+
+    #[test]
+    fn greetings_contain_name() {
+        let result = gerrting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting didn't contain name, value was '{}'",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
